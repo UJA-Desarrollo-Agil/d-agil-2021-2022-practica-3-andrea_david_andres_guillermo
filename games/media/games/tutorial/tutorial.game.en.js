dcf81287d32f1a75296db6be2bc27d7896ee9b40
g2apc00085pr4 <apc00085@red.ujaen.es>
2022-04-27 09:42:02 +0200

    Actualizacion - variables-fragemnto1,2,3

--- games/media/games/tutorial/tutorial.game.en.js
+++ games/media/games/tutorial/tutorial.game.en.js
@@ -583,13 +583,13 @@ undum.game.qualities = {
         "M.Rusa arreglada", {priority:"0002", group:'stats', onDisplay:"✓"}
     ),
     fragmento1: new undum.OnOffQuality(
-        "fr1", {priority:"0002", group:'llave'}
+        "Fragmento 1", {priority:"0002", group:'llave',onDisplay:"✓"}
     ),
     fragmento2: new undum.OnOffQuality(
-        "fr2", {priority:"0002", group:'llave'}
+        "Fragmento 2", {priority:"0002", group:'llave',onDisplay:"✓"}
     ),
     fragmento3: new undum.OnOffQuality(
-        "fr3", {priority:"0002", group:'llave'}
+        "Fragmento 3", {priority:"0002", group:'llave',onDisplay:"✓"}
     ),
 
 };
